3e678d038365865a2e2490797f5bfe3790c85115
JefferssonHe <jefferssonsaar@gmail.com>
2022-04-25 19:04:27 -0500

    primerscope

---
+++ global.js
@@ -0,0 +1,13 @@
+var hello="HELLO";
+let world="HELLO WORLD";
+const helloWorld="Hello World";
+console.log("------RUN------");
+
+
+const anotherf=()=>{
+  console.log(hello);
+  console.log(world);
+  console.log(helloWorld);
+
+}
+anotherf();
